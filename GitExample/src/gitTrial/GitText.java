9adac9f8e5e0f3743addad274d1726fe538460bc
Miryam Mansori Amar <miryam.mansori.amar@gmail.com>
2020-11-26 00:08:19 +0100

    Initial commit of project.

---
+++ GitExample/src/gitTrial/GitText.java
@@ -0,0 +1,10 @@
+package gitTrial;
+
+public class GitText {
+
+	public static void main(String[] args) {
+		System.out.println("This is a proyect example for Git");
+
+	}
+
+}
---
+++ GitExample/src/gitTrial/GitText2.java
@@ -0,0 +1,10 @@
+package gitTrial;
+
+public class GitText2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is a second proyect example for Git");
+
+	}
+
+}
